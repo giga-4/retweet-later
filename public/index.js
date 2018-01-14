95142a6439d739a762fe37839ae4a940e7d3865e
hkdnet <satoko.itse@gmail.com>
2018-01-14 16:09:35 +0900

    /bookingsの要素数だけ回すようにする

--- public/index.js
+++ public/index.js
@@ -6,11 +6,13 @@ $(function() {
     .done(function(res) {
         debugger;
         console.log(res);
-        var $tr = $('<tr>')
-        $tr.append($('<td>').text('url'))
-        $tr.append($('<td>').text('scheduled_at'))
-        $tr.append($('<td>').text('status'))
-        $('#bookings').append($tr);
+        res.forEach(function(e) {
+          var $tr = $('<tr>')
+          $tr.append($('<td>').text('url'))
+          $tr.append($('<td>').text('scheduled_at'))
+          $tr.append($('<td>').text('status'))
+          $('#bookings').append($tr);
+        });
         debugger;
     })
 });
