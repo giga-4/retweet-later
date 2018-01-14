d7f6e38014748375852a47d781e7ec73057939b5
松村和輝 <k.matsumura0314@gmail.com>
2018-01-14 16:05:07 +0900

    bookingsテーブルを追加

--- public/index.js
+++ public/index.js
@@ -4,7 +4,14 @@ $(function() {
         cache: false,
     })
     .done(function(res) {
+        debugger;
         console.log(res);
+        $('#bookings').append(
+            '<tr>' +
+                '<th>url</th>' +
+                '<th>scheduled_at</th>' +
+                '<th>status</th>' +
+            '</tr>');
         debugger;
     })
 });
