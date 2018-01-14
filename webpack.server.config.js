59a8490038e4eec38693c48577ff2a324b528742
hkdnet <satoko.itse@gmail.com>
2018-01-15 04:52:20 +0900

    Add webpack.config.js

---
+++ webpack.server.config.js
@@ -0,0 +1,58 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  target: 'node',
+  node: {
+    __dirname: true,
+  },
+  entry: './src/server/index.ts',
+  output: {
+    path: path.resolve(__dirname, './dist/server'),
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          appendTsSuffixTo: [/\.vue$/],
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
