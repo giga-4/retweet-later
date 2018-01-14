0b914e6b84a7ec687bea89a7d72a533b853844b6
松村和輝 <k.matsumura0314@gmail.com>
2018-01-14 14:11:50 +0900

    post /books

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/books', function(req, res) {
+  console.log(req.body);
+  res.redirect('/success.html');
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
