973389a59cc377a659a4ceda7dab817cf3297c8f
hkdnet <satoko.itse@gmail.com>
2018-01-15 05:41:50 +0900

    Fix

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,19 +1,18 @@
-import * as $ from 'jquery'
-import {Booking} from '../common/types'
+import * as $ from "jquery";
+import { Booking } from "../common/types";
 
 $(function() {
-    var $bookings = $('#bookings')
-    $.get({
-        url: '/bookings',
-        cache: false,
-    })
-    .done(function(res: Booking[]) {
-        res.forEach(function(e) {
-          var $tr = $('<tr>')
-          $tr.append($('<td>').text(e.url))
-          $tr.append($('<td>').text(e.scheduled_at))
-          $tr.append($('<td>').text(e.status))
-          $bookings.append($tr);
-        });
-    })
+  var $bookings = $("#bookings");
+  $.get({
+    url: "/bookings",
+    cache: false
+  }).done(function(res: Booking[]) {
+    res.forEach(function(e) {
+      var $tr = $("<tr>");
+      $tr.append($("<td>").text(e.url));
+      $tr.append($("<td>").text(e.scheduled_at));
+      $tr.append($("<td>").text(e.status));
+      $bookings.append($tr);
+    });
+  });
 });
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,20 +1,20 @@
-import * as moment from 'moment'
-import * as express from 'express'
+import * as moment from "moment";
+import * as express from "express";
 const app = express();
-import * as bodyParser from 'body-parser'
-import {Booking} from '../common/types'
-import * as mysql from 'mysql'
+import * as bodyParser from "body-parser";
+import { Booking } from "../common/types";
+import * as mysql from "mysql";
 const connection = mysql.createConnection({
-  host     : 'db',
-  user     : 'root',
-  password : 'password',
-  database : 'retweet_later'
+  host: "db",
+  user: "root",
+  password: "password",
+  database: "retweet_later"
 });
 connection.connect();
 const status = {
-  'FAILED'  : -1,
-  'WAITING' : 0,
-  'COMPLETE': 1,
+  FAILED: -1,
+  WAITING: 0,
+  COMPLETE: 1
 };
 Object.freeze(status);
 
@@ -25,42 +25,42 @@ interface BookingRecord {
   status: number;
 }
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/bookings', function(req, res) {
-  const query = 'INSERT INTO bookings SET ?';
+app.post("/bookings", function(req, res) {
+  const query = "INSERT INTO bookings SET ?";
   const options = {
     url: req.body.url,
     scheduled_at: req.body.scheduled_at,
-    status: status.WAITING,
-  }
-  connection.query(query, options, function (error, results, fields) {
+    status: status.WAITING
+  };
+  connection.query(query, options, function(error, results, fields) {
     if (error) throw error;
-    res.redirect('/success.html');
+    res.redirect("/success.html");
   });
 });
 
 const formatBooking = function(booking: BookingRecord): Booking {
-  const statusIdx = Object.values(status).indexOf(booking.status)
+  const statusIdx = Object.values(status).indexOf(booking.status);
   const ret = {
     id: booking.id,
     url: booking.url,
-    scheduled_at: moment(booking.scheduled_at).format('YYYY/MM/DD HH:mm:SS'),
-    status: Object.keys(status)[statusIdx],
-  }
-  return ret
-}
+    scheduled_at: moment(booking.scheduled_at).format("YYYY/MM/DD HH:mm:SS"),
+    status: Object.keys(status)[statusIdx]
+  };
+  return ret;
+};
 
-app.get('/bookings', function(req, res) {
-  const query = 'SELECT * FROM bookings ORDER BY `scheduled_at` ASC LIMIT 100;';
+app.get("/bookings", function(req, res) {
+  const query = "SELECT * FROM bookings ORDER BY `scheduled_at` ASC LIMIT 100;";
   connection.query(query, function(error, results, fields) {
     if (error) throw error;
     res.json(results.map(formatBooking));
   });
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000!");
 });
